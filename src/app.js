c1bcd32cc3b4e3f54383c884d53936e71d565527
sebas-masia <sebastianmasia@gmail.com>
2024-05-26 11:57:29 -0400

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,53 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  var suits = ["♠", "♥", "♦", "♣"];
+
+  var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  var randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  document.querySelector(".top-left").textContent = randomSuit;
+  document.querySelector(".center").textContent = randomNumber;
+  document.querySelector(".bottom-right").textContent = randomSuit;
 };
+
+document.getElementById("newCardButton").addEventListener("click", function() {
+  var numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  var suits = ["♠", "♥", "♦", "♣"];
+
+  var randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  var randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  document.querySelector(".top-left").textContent = randomSuit;
+  document.querySelector(".center").textContent = randomNumber;
+  document.querySelector(".bottom-right").textContent = randomSuit;
+});
